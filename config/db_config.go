777624547b2e97b4a65bef297d61f762ed5fd814
emot1con <arqanpurusa@gmail.com>
2025-05-31 13:20:31 +0700

    refactor

--- config/db_config.go
+++ config/db_config.go
@@ -22,7 +22,7 @@ func InitConfig() *Config {
 		DBPassword:     GetEnv("POSTGRES_PASSWORD", "psql"),
 		DBName:         GetEnv("POSTGRES_DB", "golang"),
 		JWTSecret:      GetEnv("JWT_SECRET_KEY", ""),
-		DatabaseURL:    GetEnv("DATABASE_URL", ""),
+		DatabaseURL:    os.Getenv("DATABASE_URL"),
 		AppEnvironment: GetEnv("APP_ENV", ""),
 	}
 }
