89df59fbcbfd8aa5b5b1463a4d94aedd90d3d887
emot1con <arqanpurusa@gmail.com>
2025-05-31 12:00:26 +0700

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	log.Println("getting environment")
 	if err := godotenv.Load(); err != nil {
-		log.Panic("error getting environment", err)
+		log.Printf("error getting environment: %v", err)
 	}
 
 	cfg := config.InitConfig()
