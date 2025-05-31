0d3a922a66ac79efcf68f507ed3f45351a2f0991
emot1con <arqanpurusa@gmail.com>
2025-05-31 11:42:29 +0700

    refactoring

---
+++ config/db_config.go
@@ -0,0 +1,32 @@
+package config
+
+import "os"
+
+type Config struct {
+	PublicHost            string
+	Port                  string
+	DBUser                string
+	DBPassword            string
+	DBName                string
+	JWTExpirationInSecond int64
+	JWTSecret             string
+}
+
+func InitConfig() *Config {
+	return &Config{
+		PublicHost: GetEnv("PGHOST", "localhost"),
+		Port:       GetEnv("PGPORT", "5432"),
+		DBUser:     GetEnv("PGUSER", "psql"),
+		DBPassword: GetEnv("POSTGRES_PASSWORD", "psql"),
+		DBName:     GetEnv("POSTGRES_DB", "golang"),
+		JWTSecret:  GetEnv("JWT_SECRET_KEY", ""),
+	}
+}
+
+func GetEnv(key, fallback string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	return value
+}
